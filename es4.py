53a71a5919edc0f29f47fd2736043b8a9ce1eab0
repl.it user <replituser@example.com>
2020-11-17 20:26:37 +0000

    Leggere file .csv attraverso Classe Oggetto

---
+++ es4.py
@@ -0,0 +1,28 @@
+
+class CSVFile:
+
+    def __init__(self,name):
+        self.name = name
+
+    def get_data(self):
+
+        values = []
+
+        my_file = open(self.name,"r")
+
+        for line in my_file:
+
+            elements = line.split(',')   
+
+            if elements[0] != 'Date':   
+
+                    date = elements[0]
+                    value = elements[1]
+                    values.append(float(value))
+
+        return values
+
+mio_file = CSVFile(name='shampoo_sales.csv')
+
+print(mio_file.name)
+print(mio_file.get_data())
